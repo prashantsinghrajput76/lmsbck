78a16e1ea7f0ad250491a9cce873fe41de4d8b43
Prashant Kumar Singh <singhprashant787@gmail.com>
2025-05-23 23:47:57 +0530

    wdf

--- server/index.js
+++ server/index.js
@@ -23,10 +23,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://lmsfnd-6p39.vercel.app/"
+];
  
 // apis
 app.use("/api/v1/media", mediaRoute);
